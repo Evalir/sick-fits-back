fd207fe26f20bcecd5576bca455d4921bb82b77f
evalir <hi.evalir@gmail.com>
2019-06-11 18:39:30 -0400

    Lesson 14

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
 
-module.exports = mutations;
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they're logged in.
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+    return item;
+  }
+};
+
+module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const {forwardTo} = require('prisma-binding');
+const Query = {
+  items: forwardTo('db'),
+  // async Items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
