ea5dd3ee304d83419405a01ceb5c5848e591cffc
evalir <hi.evalir@gmail.com>
2019-06-14 17:04:21 -0400

    Until 1/2 lesson 20

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,3 @@
-
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they're logged in.
@@ -6,7 +5,36 @@ const Mutations = {
       data: { ...args }
     }, info);
     return item;
-  }
+  },
+  async updateItem(parent, args, ctx, info) {
+    // first take a copy of the items
+    const updates = {...args};
+    // remove updates from id
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id,
+      },
+    }, info);
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = {id: args.id};
+    // 1. Find item
+    const item = await ctx.db.query.item(
+      {where}, 
+      `{ 
+        id
+        title
+      }`
+    );
+    // 2. Check if they own that item, or have the permissions
+    //TODO
+    // 3. Delete it.
+    return ctx.db.mutation.deleteItem({where}, info);
+  },
 };
 
 module.exports = Mutations;
